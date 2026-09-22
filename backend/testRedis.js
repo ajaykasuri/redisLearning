c272a80572fc9794e4570c8841a2ea2b673fab74
Ajay Kasuri <ajay.kasuri@globalsparktek.com>
2026-09-22 15:18:39 +0530

    redis crud basic

--- backend/testRedis.js
+++ backend/testRedis.js
@@ -1,5 +1,7 @@
 const redisClient=require('./config/redis')
 
+
+{/*storing the data*/}
 redisClient.set("username","ajay")
 .then(()=>{
     console.log("Data stored successfully")
@@ -8,27 +10,32 @@ redisClient.set("username","ajay")
     console.log("Error while storing the data:",err)
 })
 
-// redisClient.get("username")
-// .then((username)=>{
-//     console.log("username:",username)
-// })
-// .catch((err)=>{
-//     console.log("Error while getting the data:",err)
-// })
+{/*get stored data*/}
 
+redisClient.get("username")
+.then((username)=>{
+    console.log("username:",username)
+})
+.catch((err)=>{
+    console.log("Error while getting the data:",err)
+})
 
-// redisClient.del("username")
-// .then((res)=>{
-// if(res===1){
-//     console.log("deleted successfully")
-// }
-// else{
-//     console.log("didn't exist")
-// }
-// })
-// .catch((err)=>{
-//     console.log("Error while deleting the data:",err)
-// })
+{/*delete stored data*/}
+
+redisClient.del("username")
+.then((res)=>{
+if(res===1){
+    console.log("deleted successfully")
+}
+else{
+    console.log("didn't exist")
+}
+})
+.catch((err)=>{
+    console.log("Error while deleting the data:",err)
+})
+
+{/*check stored data exists or not*/}
 
 redisClient.exists("username")
 .then((res)=>{
